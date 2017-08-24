2d7076384a89f1333f5bd4f5423dd13e3814d056
Daniela-Villanueva <30873735+Daniela-Villanueva@users.noreply.github.com>
2017-08-24 17:21:21 -0500

    Add files via upload

---
+++ Prueba de ordenación de un conjunto.py
@@ -0,0 +1,24 @@
+Python 3.5.4 (v3.5.4:3f56838, Aug  8 2017, 02:07:06) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import random
+p=[]
+
+   for i in range(100):
+    p.append(i)
+
+def minimo(arr):
+   aux=arr   #creo el arreglo
+   for a in arr: #para cada elemento del arreglo/conjunto
+   aux.pop(a): #remuevo a del conjunto auxiliar 
+   for c in aux: #para cada elemento c del conjunto auxiliar
+
+printf(a,c)
+if(a>c): #comoparo si a es mayor a c
+break #si lo es, salgo para probar el siguiente a, si no, comparo con el siguiente c
+resultado=a
+break
+return resultado 
+random.shuffle(p)
+
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
