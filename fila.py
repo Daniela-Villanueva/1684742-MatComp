0f609d58cae0722dcbb002eb50fd9320b8c9351f
Daniela-Villanueva <30873735+Daniela-Villanueva@users.noreply.github.com>
2017-09-20 11:53:46 -0500

    Add files via upload

---
+++ fila.py
@@ -0,0 +1,132 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> class pila:
+	def_init_(self):
+		
+SyntaxError: invalid syntax
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+
+		
+>>> 
+>>> 
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(self,e):
+		self.pila.append(e)
+		return len(self,pila)
+
+	
+>>> p=pila()
+>>> p.meter(1)
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    p.meter(1)
+  File "<pyshell#14>", line 7, in meter
+    self.pila.append(e)
+AttributeError: 'pila' object has no attribute 'pila'
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(self,e):
+		self.pila.append(e)
+		return len(self,pila)
+
+	
+>>> p=pila()
+>>> pclass pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(self,e):
+		self.pila.append(e)
+		return len(self,pila)
+	
+SyntaxError: invalid syntax
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(self,e):
+		self.pila.append(e)
+		return len(self,pila)
+	def longitud(self):
+		return len(self.pila)
+
+	
+>>> p=pila()
+>>> p.meter(1)
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    p.meter(1)
+  File "<pyshell#24>", line 7, in meter
+    self.pila.append(e)
+AttributeError: 'pila' object has no attribute 'pila'
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(self,e):
+		self.pila.append(e)
+		return len(self,pila)
+	@property
+	def longitud(self):
+		return len(self.pila)
+
+>>> p=pila()
+>>> p.meter(1)
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    p.meter(1)
+  File "<pyshell#29>", line 7, in meter
+    self.pila.append(e)
+AttributeError: 'pila' object has no attribute 'pila'
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(self,e):
+		insert(0,e)
+		return len(self,pila)
+	@property
+	def longitud(self):
+		return len(self.pila)
+
+	
+>>> p=pila()
+>>> p.meter(1)
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    p.meter(1)
+  File "<pyshell#36>", line 7, in meter
+    insert(0,e)
+NameError: name 'insert' is not defined
+>>> class pila:
+	def _init_(self):
+		self.pila = []
+	def obtener(self):
+		return self.pila.pop()
+	def meter(insert (0,e)):
+		
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> def fila:
+	
+SyntaxError: invalid syntax
+>>> class fila:
+	def _init_(self):
+		self.fila=[]
+	def obtener(self):
+		return self.fila.pop()
+	
